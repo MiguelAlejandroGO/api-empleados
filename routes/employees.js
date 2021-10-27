5f68ac4afdfdd7f72104cb55aa30e56f7afac00e
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-10-27 00:19:26 -0500

    Update employees.js

--- routes/employees.js
+++ routes/employees.js
@@ -2,22 +2,22 @@ const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
+const mysqlConnection = require('../database.js');
 
 // GET all Employees
 router.get('/', (req, res) => {
   mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) => {
-    if(!err) {
+    if (!err) {
       res.json(rows);
     } else {
       console.log(err);
     }
-  });  
+  });
 });
 
 // GET An Employee
 router.get('/:id', (req, res) => {
-  const { id } = req.params; 
+  const { id } = req.params;
   mysqlConnection.query('SELECT * FROM employee WHERE id = ?', [id], (err, rows, fields) => {
     if (!err) {
       res.json(rows[0]);
@@ -31,8 +31,8 @@ router.get('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
   mysqlConnection.query('DELETE FROM employee WHERE id = ?', [id], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Employee Deleted'});
+    if (!err) {
+      res.json({ status: 'Employee Deleted' });
     } else {
       console.log(err);
     }
@@ -41,39 +41,52 @@ router.delete('/:id', (req, res) => {
 
 // INSERT An Employee
 router.post('/', (req, res) => {
-  const {id, name, salary} = req.body;
+  const { id, name, salary } = req.body;
 
-  if ( id == null || name == null || salary == null){
+  if (id === null || name === null || salary === null) {
     res.status(400).json({ datos: 'Todos los campos son obligatorios' });
-  }else{
+  } else {
     const query = `
     CALL employeeAddOrEdit(?, ?, ?);
   `;
-  mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Employeed Saved'});
-    } else {
-      console.log(err);
-      console.log('Error');
-    }
-  });
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: 'Employeed Saved' });
+      } else {
+        console.log(err);
+        console.log('Error');
+      }
+    });
   }
-  
+
 });
 
 router.put('/:id', (req, res) => {
   const { name, salary } = req.body;
   const { id } = req.params;
-  const query = `
+
+  if (id === undefined || id === null) {
+    res.status(400).json({ datos: 'Usuarios no encontrado' });
+
+  } else {
+
+    if (name === null || salary === null) {
+      res.status(400).json({ datos: 'Todos los campos son obligatorios' });
+    } else {
+      const query = `
     CALL employeeAddOrEdit(?, ?, ?);
   `;
-  mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Employee Updated'});
-    } else {
-      console.log(err);
+      mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if (!err) {
+          res.json({ status: 'Employee Updated' });
+        } else {
+          console.log(err);
+        }
+      });
     }
-  });
+
+  }
+
 });
 
 module.exports = router;
