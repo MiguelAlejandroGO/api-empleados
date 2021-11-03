6d57813cf4426c16f30d38e547dc5f503c4ab065
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-11-03 11:44:41 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'sql5.freemysqlhosting.net',
-  user: 'sql5447071',
-  port: 3306,
-  password: 'ffy4vKxsjC',
-  database: 'sql5447071',
+  host: 'db4free.net',
+  port : 3306,
+  user: 'gestor_empleados',
+  password: 'appEmpleados',
+  database: 'bd_appempleados',
 });
 
 mysqlConnection.connect(function (err) {
